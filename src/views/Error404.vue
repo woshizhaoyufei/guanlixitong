b1531639e87de5ffeb700d21bbd60eb853de1ed2
woshizhaoyufei <18332821053@163.com>
2020-07-04 18:29:27 +0800

    修改404

--- src/views/Error404.vue
+++ src/views/Error404.vue
@@ -1,16 +1,13 @@
 <template>
-    <div>
-        <h1>无页面</h1>
-        <p @click="$router.go(-1)">跳转到上一页</p>
-    </div>
+  <div>
+    <h1>无此页面</h1>
+    <p @click="$router.go(-1)">跳转到上一页</p>
+  </div>
 </template>
 
 <script>
-    export default {
-        
-    }
+export default {};
 </script>
 
 <style lang="less" scoped>
-
 </style>
